1c19ec7f86c7bce5a52304939873d8d51770b217
Ronei Macedo Gualberto <ronei.mac.rmg@gmail.com>
2024-01-14 09:01:30 -0400

    Include Cache Mecanism

--- tiny-url-webapi/src/app.module.ts
+++ tiny-url-webapi/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [],
+  imports: [
+    CacheModule.register({
+      ttl: 5, // seconds
+      max: 10, // maximum number of items in cache
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
