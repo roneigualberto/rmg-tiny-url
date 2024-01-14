6adae895c80d037d0f03fe2313bc3e3458c2ff07
Ronei Macedo Gualberto <ronei.mac.rmg@gmail.com>
2024-01-14 09:01:47 -0400

    API Version Setup

--- tiny-url-webapi/src/main.ts
+++ tiny-url-webapi/src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: 'v1',
+    prefix: 'api/',
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Tiny URL API')
