5f8d17a8ff86e9477ebb1364c66c422c4cf0cb72
Ronei Macedo Gualberto <ronei.mac.rmg@gmail.com>
2024-01-14 08:31:12 -0400

    Open API Documention

--- tiny-url-webapi/src/main.ts
+++ tiny-url-webapi/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+  const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Tiny URL API')
+    .setDescription('Tiny URL API documention')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(port);
 }
 bootstrap();
